727ca53e941737094d723f7748c7b40b9e041dc6
Daniel Narey <danielnarey@users.noreply.github.com>
2019-11-12 11:56:14 -0500

    sync

--- test/src.js
+++ test/src.js
@@ -1,5 +1,43 @@
+import path from 'path';
 import test from 'ava';
+import listFiles from '../src/list-files';
 import pathToName from '../src/path-to-name';
+
+
+const contentPath = path.resolve(__dirname, './content');
+
+
+test('listFiles', (t) => {
+  t.deepEqual(
+    listFiles(contentPath),
+    [
+      'post-1.md',
+      'post-2.md',
+      'post-3.md',
+      'random',
+      'something.txt',
+    ].map((x) => path.resolve(contentPath, x)),
+  );
+  
+  t.deepEqual(
+    listFiles(contentPath, 'md'),
+    [
+      'post-1.md',
+      'post-2.md',
+      'post-3.md',
+    ].map((x) => path.resolve(contentPath, x)),
+  );
+  
+  t.deepEqual(
+    listFiles(contentPath, ['md', 'txt']),
+    [
+      'post-1.md',
+      'post-2.md',
+      'post-3.md',
+      'something.txt',
+    ].map((x) => path.resolve(contentPath, x)),
+  );
+});
 
 
 test('pathToName', (t) => {

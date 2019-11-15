85e3b949513ca2fcb116cf67ec0e4468841d03a4
danielnarey <accounts@danielnarey.com>
2019-11-15 00:50:07 +0000

    automated sync

--- src/build-html.js
+++ src/build-html.js
@@ -17,35 +17,35 @@ const buildHtml = async (context, {
   templates = 'templates',
 } = {}) => {
   const templatesDir = path.resolve(context, './${src}', './${templates}');
-  
+
   throwIf(
     !(await fs.pathExists(templatesDir)),
     () => `Looking for entry template, but ${templatesDir} does not exist`,
-  ); 
-  
+  );
+
   const extensions = ['html', 'pug'];
   const indexFile = listFiles(templatesDir, extensions, 'index');
-  
+
   throwIf(
     indexFile.length === 0,
     () => `No entry file named "index.html" or "index.pug" was found in ${templatesDir}`,
   );
-  
+
   throwIf(
     indexFile.length > 1,
     () => `Multiple entry files names "index" were found in ${templatesDir}. Rename each one that is not the entry template.`,
   );
-  
+
   const content = mapToObject(importContent(context));
   const data = mapToObject(importData(context));
   const locals = { content, ...data };
-  
+
   const html = await caseOf(ext(indexFile), [
     ['html', () => minifyHtml(indexFile)],
-    ['jstl', () => renderJstl(indexFile, locals)], 
+    ['jstl', () => renderJstl(indexFile, locals)],
     ['pug', () => renderPug(templatesDir, 'index.pug', locals)],
   ])();
-  
+
   const out = path.resolve(context, './${dist}', './${name}.html');
 
   await fs.outputFile(out, `${html}\n`);

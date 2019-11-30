36205068a199286939227789e71e9a09dd04e92c
Daniel Narey <danielnarey@users.noreply.github.com>
2019-11-30 14:16:30 -0500

    sync

--- cli.js
+++ cli.js
@@ -1,11 +1,16 @@
 #!/usr/bin/env node
 
+import path from 'path';
 import mri from 'mri';
 import index from './index';
 
 
 (() => {
   const cliArgs = mri(process.argv.slice(2));
+  const context = path.resolve(
+    process.cwd(),
+    cliArgs._[0] || '',
+  );
 
-  index(cliArgs._[0]);
+  index(context);
 })();

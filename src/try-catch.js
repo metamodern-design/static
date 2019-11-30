d933fed16e72d2f373a1809f8b697e3939e23c7e
Daniel Narey <danielnarey@users.noreply.github.com>
2019-11-30 15:28:50 -0500

    sync

--- src/try-catch.js
+++ src/try-catch.js
@@ -1,13 +1,10 @@
-import cleanStack from 'clean-stack';
-
-
 const tryCatch = async (call, msg) => {
   try {
     const result = await call();
     return result || 0;
   } catch (err) {
     const newErr = new Error(msg(err));
-    console.error(cleanStack(newErr.stack));
+    console.error(newErr.stack);
     return 1;
   }
 };

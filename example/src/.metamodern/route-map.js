b97ee4c9f89b9298c5d898ecc5ebc579af82aeed
danielnarey <accounts@danielnarey.com>
2019-12-01 04:19:59 +0000

    automated sync

---
+++ example/src/.metamodern/route-map.js
@@ -0,0 +1,2 @@
+const routeMap = new Map([["/tinysite-layout-stacked/","section#about\n  header\n    hr\n    h1 About\n\n  .grid\n    .cell.flex-half.scale-text\n      != content.about\n"]]);
+export default routeMap;

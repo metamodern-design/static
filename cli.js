2c746a0abef0706a15116e47d3cac744c72db7f2
Daniel Narey <danielnarey@users.noreply.github.com>
2019-12-06 21:50:18 -0500

    sync

--- cli.js
+++ cli.js
@@ -4,24 +4,30 @@ import path from 'path';
 import fs from 'fs-extra';
 import mri from 'mri';
 import index from './index';
+import tryCatch from './src/try-catch';
 
 
-(async () => {
-  const cliArgs = mri(process.argv.slice(2));
-  const context = path.resolve(
-    process.cwd(),
-    cliArgs._[0] || '',
+(() => {
+  tryCatch(
+    async () => {
+      const cliArgs = mri(process.argv.slice(2));
+      const context = path.resolve(
+        process.cwd(),
+        cliArgs._[0] || '',
+      );
+    
+      const configPath = path.resolve(context, 'metamodern.config.js');
+    
+      const config = (
+        await fs.pathExists(configPath)
+          ? await import(configPath)
+          : {}
+      );
+    
+      const outputPaths = await index(context, config);
+    
+      console.log(`Files generated:\n${outputPaths.join('\n')}`);
+    },
+    (err) => `Build failed: ${err}`,
   );
-
-  const configPath = path.resolve(context, 'metamodern.config.js');
-
-  const config = (
-    await fs.pathExists(configPath)
-      ? await import(configPath)
-      : {}
-  );
-
-  const outputPaths = await index(context, config);
-
-  console.log(`Files generated:\n${outputPaths.join('\n')}`);
 })();

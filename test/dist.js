890bfcde1a9d99a6b55a72ea29a3cb9ece2a0029
Daniel Narey <danielnarey@users.noreply.github.com>
2019-12-18 13:45:10 -0500

    sync

--- test/dist.js
+++ test/dist.js
@@ -1,4 +1,5 @@
 import path from 'path';
+import del from 'del';
 import fs from 'fs-extra';
 import test from 'ava';
 import index from '../index';
@@ -8,10 +9,24 @@ const example = path.resolve(__dirname, 'example');
 
 
 test('build script generated the public files', async (t) => {
+  await del(`${example}/dist/*`);
   await index(example);
 
   t.true((await Promise.all([
     fs.exists(path.resolve(example, 'dist/index.html')),
+    fs.exists(path.resolve(example, 'dist/index.css')),
+    fs.exists(path.resolve(example, 'dist/index.js')),
+    fs.exists(path.resolve(example, 'dist/media/logo.svg')),
+    fs.exists(path.resolve(example, 'dist/404.html')),
+  ])).every((x) => x));
+});
+
+
+test('skipHtml option', async (t) => {
+  await del(`${example}/dist/*`);
+  await index(example, { skipHtml: true });
+
+  t.true((await Promise.all([
     fs.exists(path.resolve(example, 'dist/index.css')),
     fs.exists(path.resolve(example, 'dist/index.js')),
     fs.exists(path.resolve(example, 'dist/media/logo.svg')),

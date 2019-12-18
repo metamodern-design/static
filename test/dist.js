e090e4d469ccf5b1166c1ff3ea2306966d2b4059
Daniel Narey <danielnarey@users.noreply.github.com>
2019-12-18 14:51:10 -0500

    sync

--- test/dist.js
+++ test/dist.js
@@ -8,13 +8,13 @@ import index from '../index';
 const example = path.resolve(__dirname, 'example');
 
 
-test.beforeEach(async (t) => {
-  await del(`${example}/dist/*`);
+test.beforeEach(() => {
+  del.sync(`${example}/dist/*`);
 });
 
 
 test.after(async (t) => {
-  await del(`${example}/dist/*`);
+  del.sync(`${example}/dist/*`);
 });
 
 /*
@@ -37,10 +37,11 @@ test('skipHtml option', async (t) => {
   console.log(built);
 
   t.true((await Promise.all([
-    fs.exists(path.resolve(example, 'dist/index.html')),
     fs.exists(path.resolve(example, 'dist/index.css')),
     fs.exists(path.resolve(example, 'dist/index.js')),
     fs.exists(path.resolve(example, 'dist/media/logo.svg')),
     fs.exists(path.resolve(example, 'dist/404.html')),
   ])).every((x) => x));
+  
+  t.false(await fs.exists(path.resolve(example, 'dist/index.html')));
 });
